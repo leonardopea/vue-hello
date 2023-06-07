5fcacfe6b301db39bb7479b84a605292c51a845c
leonardopea <leonardo.pea.1996@gmail.com>
2023-06-08 00:21:42 +0200

    ex bonus (immagine) completato

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ createApp({
 
     data(){
         return{
-            message: '<h1>Ecco il mio esercizio</h1>'
-        }
+            message: '<h1>Ecco il mio esercizio</h1>',
+            image: 'https://picsum.photos/200/300',
+            
     }
 
-
+}
 }).mount('#app');
